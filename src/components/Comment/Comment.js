b0c48fcb96e77b8ed999a22f36360a5dcf2ee21f
junki <junki646@gmail.com>
2022-08-05 16:24:40 +0900

    댓글 좋아요, 좋아요 취소 구현, 컴퍼넌트 구조 변경

--- src/components/Comment/Comment.js
+++ src/components/Comment/Comment.js
@@ -1,22 +1,23 @@
 import React from 'react'
 import './Comment.scss'
-
-
+import { useState } from 'react'
 
 const Comment = (props) => {
+    const [heartSrc, setHeartSrc] = useState([true, './images/heart.png'])
+
+    function commentHeart(e) {
+        heartSrc[0] ? setHeartSrc([false, './images/redHeart.png']) : setHeartSrc([true, './images/heart.png'])
+    }
+
     return (
-        props.comments.map((comment, idx) => {
-            return (
-                <div className="commentBox" key={idx}>
-                    <div className="comment">
-                        <span className="userName">{props.nickname || 'nyam_nyam2'}</span>{comment}
-                    </div>
-                    <div className="commentImgBox">
-                        <img src="./images/heart.png" className="commentHeart" alt="tmp" />
-                    </div>
-                </div>
-            )
-        })
+        <div className="commentBox" key={props.idx}>
+            <div className="comment">
+                <span className="userName">{props.nickname || props.myName}</span>{props.comment}
+            </div>
+            <div className="commentImgBox">
+                <img onClick={(e) => commentHeart(e)} src={heartSrc[1]} className="commentHeart" alt="tmp" />
+            </div>
+        </div>
     )
 }
 
--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -6,12 +6,7 @@ import ProfileBox from '../../components/ProfileBox/ProfileBox';
 import StoryBox from '../../components/StoryBox/StoryBox';
 import RecommendBox from '../../components/RecommendBox/RecommendBox';
 
-// commentButton.forEach((btn, idx) => btn.addEventListener('click', () => commentAdd(idx)))
 
-// commentInput.forEach((input, idx) => input.addEventListener('keyup', (key) => {
-//     if (key.key === 'Enter') commentAdd(idx)
-//     commentButtonStyle(idx)
-// }))
 
 // function commentButtonStyle(idx) {
 //     if (commentInput[idx].value.length > 0) {
@@ -20,61 +15,15 @@ import RecommendBox from '../../components/RecommendBox/RecommendBox';
 //     } else {
 //         commentButton[idx].style = '';
 //     }
-// }
+// }3
 
-// function commentAdd(idx) {
-//     if (commentInput[idx].value == '') return;
-//     const commentBoxDiv = document.createElement('div')
-//     const commentDiv = document.createElement('div')
-//     const userNameSpan = document.createElement('span')
-//     const meEngName = document.querySelector(".meEngName")
-//     const imgDiv = document.createElement('div')
-//     const closeImgTag = document.createElement('img')
-//     const heartImgTag = document.createElement('img')
-
-
-//     commentBoxDiv.classList.add('commentBox')
-//     commentDiv.classList.add('comment')
-//     userNameSpan.classList.add('userName')
-//     closeImgTag.classList.add('commentClose')
-//     heartImgTag.classList.add('commentHeart')
-//     imgDiv.classList.add('commentImgBox')
 
 //     closeImgTag.src = './img/close.png'
 //     heartImgTag.src = './img/heart.png'
 //     userNameSpan.innerText = meEngName.innerText
 
-//     imgDiv.appendChild(closeImgTag)
-//     imgDiv.appendChild(heartImgTag)
-//     commentDiv.appendChild(userNameSpan)
-//     commentBoxDiv.appendChild(commentDiv)
-//     commentBoxDiv.appendChild(imgDiv)
 
-//     commentDiv.innerHTML += commentInput[idx].value
-//     commentContainer[idx].appendChild(commentBoxDiv);
-//     commentInput[idx].value = '';
-//     commentButtonStyle(idx)
-//     closeImgTag.addEventListener('click', ()=>commentClose(commentBoxDiv,idx))
-//     heartImgTag.addEventListener('click', ()=>commentHeart(heartImgTag))
 
-//     commentCount(idx)
-// }
-
-// const cssHeart = document.querySelectorAll('.commentHeart')
-// cssHeart.forEach((heart) => heart.addEventListener('click', () => commentHeart(heart)))
-
-// function commentClose(commentBoxDiv,idx) {
-//     commentBoxDiv.remove()
-//     commentCount(idx)
-// }
-
-// function commentHeart(heart){
-//     if(heart.classList.contains('commentRedHeart')){
-//         heart.classList.remove('commentRedHeart')
-//     }else{
-//         heart.classList.add('commentRedHeart')
-//     }
-// }
 
 // const commentMore = document.querySelectorAll('.commentMore')
 // commentMore.forEach((_, idx)=>commentCount(idx))
